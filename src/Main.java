3b4a6ef1f4bbf3729da116cf600d67f7b146e859
Dima <dmitriy.matveev89@inbox.ru>
2022-11-22 17:30:04 +0300

    Ver 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 /*
 1. Создайте массив из мнемонической фразы: Каждый охотник желает знать, где сидит фазан.
 2. Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
 */
-
-        
+        String lvText = "Каждый охотник желает знать, где сидит фазан";
+        String[] ltText = lvText.split(",^?\\s+");
+        for (int i = 0; i < ltText.length; i++) {
+            System.out.println(ltText[i]);
+        }
     }
 }
